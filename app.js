8398a6b9b6c80341a20c4f9a61ad5407a50bbcff
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-04-17 00:38:04 +0900

    add main and inventory router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var invenRouter = require("./routes/inventory");
 
 var app = express();
 
@@ -14,12 +15,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/inventory", invenRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
