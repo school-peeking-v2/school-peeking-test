f33d1eb93a08cee01acf680a344a6137660e05d7
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-04-19 04:49:59 +0900

    firebase init

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const firebase = require("../firebase-config");
+const auth = require("firebase/auth");
+
 router.get("/", (req, res) => {
   console.log("connected");
   res.render("index");
